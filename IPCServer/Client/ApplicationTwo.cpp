407bc9fd7fe766b733e1569e8fc7a01873da07cf
Blake Peck <s210561@students.aie.edu.au>
2021-07-12 16:15:12 +1000

    up to the part where i followed the code but now lost

--- IPCServer/Client/ApplicationTwo.cpp
+++ IPCServer/Client/ApplicationTwo.cpp
@@ -1,17 +1,49 @@
 #include <Windows.h>
+#include <iostream>
+#include <conio.h>
 
 struct MyData
 {
 	int i;
-	float f;
-	char c;
-	bool b;
-	double d;
+	//float f;
+	//char c;
+	//bool b;
+	//double d;
 };
 
 int main()
 {
 	HANDLE fileHandle = OpenFileMapping(FILE_MAP_ALL_ACCESS, false, L"MySharedMemoty");
 
+	if (fileHandle == nullptr)
+	{
+		std::cout << "could not create file mapping object: " << GetLastError() << std::endl;
+
+		return 1;
+	}
+
+	MyData* data = (MyData*)MapViewOfFile(fileHandle, FILE_MAP_ALL_ACCESS, 0, 0, sizeof(MyData));
+
+	if (data == nullptr)
+	{
+		std::cout << "could not map view of file: " << GetLastError() << std::endl;
+		CloseHandle(fileHandle);
+		return 1;
+	}
+
+	std::cout << "MyData = {";
+	std::cout << data->i << ",";
+	//std::cout << data->f << ",";
+	//std::cout << data->c << ",";
+	//std::cout << data->b << ",";
+	//std::cout << data->d << ",";
+	std::cout << " }:" << std::endl;
+
+	//_getch();
+
+	UnmapViewOfFile(data);
+
 	CloseHandle(fileHandle);
+
+	return 0;
 }
--- IPCServer/IPCServer/ApplicationOne.cpp
+++ IPCServer/IPCServer/ApplicationOne.cpp
@@ -1,15 +1,44 @@
 #include <Windows.h>
+#include <iostream>
+#include <conio.h>
 
 struct MyData
 {
 	int i;
-	float f;
-	char c;
-	bool b;
-	double d;
+	//float f;
+	//char c;
+	//bool b;
+	//double d;
 };
 
 int main()
 {
+	MyData myData = { 10 };
 
+	HANDLE fileHandle = CreateFileMapping(INVALID_HANDLE_VALUE, nullptr, PAGE_READWRITE, 0, sizeof(MyData), L"MySharedMemory");
+
+	if (fileHandle == nullptr)
+	{
+		std::cout << "Could not create file mapping object: " << GetLastError() << std::endl;
+		return 1;
+	}
+
+	MyData* data = (MyData*)MapViewOfFile(fileHandle, FILE_MAP_ALL_ACCESS, 0, 0, sizeof(MyData));
+
+	if (data == nullptr)
+	{
+		std::cout << "could not map view of file: " << GetLastError() << std::endl;
+		CloseHandle(fileHandle);
+		return 1;
+	}
+
+	*data = myData;
+
+	//_getch();
+
+	UnmapViewOfFile(data);
+
+	CloseHandle(fileHandle);
+
+	return 0;
 }
